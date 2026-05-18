615c9518bb6de78bcfaafee171b4beb77aa73798
janvi <janvibajaj02@gmail.com>
2026-05-19 01:17:23 +0530

    Fix CORS issue on production by allowing FRONTEND_URL

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', process.env.FRONTEND_URL],
   credentials: true
 }));
 
